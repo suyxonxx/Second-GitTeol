fa2d278a44410912f5e3474e775e4b70edc0092c
suyxonxx <suyxonxx@gmail.com>
2022-05-01 22:40:46 +0900

    220501

--- JAVAjungsuk/src/CarTest3.java
+++ JAVAjungsuk/src/CarTest3.java
@@ -1,13 +1,13 @@
 class Car {
-	String color;		//색상
-	String gearType;	//변속기 종류 - auto(자동), manual(수동)
-	int door;			//문 갯수
+	String color; //색상
+	String gearType; //변속기 종류 - auto(자동), manual(수동)
+	int door; //문 갯수
 	
 	Car() {
 		this("white", "auto", 4);
 	}
 	
-	Car(Car c) {		//인스턴스 복사를 위한 생성자
+	Car(Car c) { //인스턴스 복사를 위한 생성자
 		color = c.color;
 		gearType = c.gearType;
 		door = c.door;
@@ -23,12 +23,12 @@ class Car {
 class CarTest3 {
 	public static void main(String[] args) {
 		Car c1 = new Car();
-		Car c2 = new Car(c1);	//c1의 복사본 c2 생성
+		Car c2 = new Car(c1); //c1의 복사본 c2 생성
 		System.out.println("c1의 color = " + c1.color + ", gearType = "
 								+ c1.gearType + ", door = " + c1.door);
 		System.out.println("c2의 color = " + c2.color + ", gearType = "
 								+ c2.gearType + ", door = " + c2.door);
-		c1.door = 100;			//c1의 인스턴스변수 door의 값을 변경
+		c1.door = 100; //c1의 인스턴스변수 door의 값을 변경
 		System.out.println("c1.door = 100; 수행 후");
 		System.out.println("c1의 color = " + c1.color + ", gearType = "
 								+ c1.gearType + ", door = " + c1.door);
